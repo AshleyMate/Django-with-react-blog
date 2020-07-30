795833cb0e9d2d703ecea584504fd8f3d6d94805
chanyoung <young9484825@gmail.com>
2020-07-30 21:36:12 +0900

    기본 환경 세팅

---
+++ backend/backend/settings/dev.py
@@ -0,0 +1,17 @@
+from .common import *
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+# MIDDLEWARE += [
+#     'debug_toolbar.middleware.DebugToolbarMiddleware',
+# ]
+# 이렇게 설정하면 미들웨어 제일 마지막에 추가가 된다
+
+MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+] + MIDDLEWARE
+# 이렇게 해야 미들웨어의 처음에 추가가 된다
+
+INTERNAL_IPS = ['127.0.0.1']
